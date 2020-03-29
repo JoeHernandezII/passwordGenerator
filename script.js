5c0b735996bca45c91ba0cd71586370986e095de
Joe Hernandez <yoshi.jhii@gmail.com>
2020-03-28 23:44:28 -0500

    Updated script.js so that prompts and confirms were asked of user.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//
+var passwordLength = prompt("How long do you want the password to be?");
+console.log(passwordLength);
+var lowerCase = confirm("Would you like lowercase letters?");
+console.log(lowerCase);
+var upperCase = confirm("Would you like uppercase letters?");
+console.log(upperCase);
+var numericPass = confirm("Does the password need to have numbers?");
+console.log(numericPass);
+var specialCharacters = confirm("Does the password need special characters?");
+console.log(specialCharacters);
+
 
 // Write password to the #password input
 function writePassword() {
