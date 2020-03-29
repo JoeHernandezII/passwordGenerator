7bce807c49cac701361b4e0d1c9d9cbb73879996
Joe Hernandez <yoshi.jhii@gmail.com>
2020-03-29 01:27:42 -0500

    Added to the script.js so that random lower/upper case, num, and symbols will take place if true from prompts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//
+
+//Propts user for the length and password credentials
 var passwordLength = prompt("How long do you want the password to be?");
 console.log(passwordLength);
 var lowerCase = confirm("Would you like lowercase letters?");
@@ -12,11 +13,28 @@ console.log(numericPass);
 var specialCharacters = confirm("Does the password need special characters?");
 console.log(specialCharacters);
 
+function getRandomLC() {
+  return String.fromCharCode(Math.floor(Match.random() * 26) + 97);
+}
+
+function getRandomUC() {
+  return String.fromCharCode(Math.floor(Match.random() * 26) + 65);
+}
+
+function getRandomNum() {
+  return String.fromCharCode(Math.floor(Match.random() * 26) + 48);
+}
+
+function getRandomSpecial() {
+  const symbols = '!@#$%^&*(){}[]\/?.,;:';
+  return symbols[Math.floor(Match.random() * symbols.length)];
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
